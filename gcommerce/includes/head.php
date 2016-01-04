744baeb35afcf6ce617bd2a0967b395a46c929f5
lucasmachoalfa <lucas.freitas@agenciaguppy.com.br>
2016-01-04 17:29:18 -0200

    projeto todo

---
+++ gcommerce/includes/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<title>G-COMMERCE</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<script src="js/jquery.js"></script>
+<link rel="stylesheet" type="text/css" href="btsp/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="btsp/css/bootstrap-theme.min.css"/>
+<script src="btsp/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
