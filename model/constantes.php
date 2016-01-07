16cdbba908897543f2965bcfb4d0a7e9f90ddaa5
pauloguppy <paulo.sergio@agenciaguppy.com.br>
2016-01-07 10:53:50 -0200

    arquivos de conexão com o banco

---
+++ model/constantes.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'constantesBanco.php';
+
+//função para remover expressões comuns de banco de dados
+function seg($var) {
+    $procura = array("insert into", "update", "delete from", "select % from");
+    $retorno = str_ireplace($procura, '', $var);
+//    $retorno = htmlentities($retorno);
+    
+    return $retorno;
+}
+
+//Constantes de Tabela
+//Tabelas ADMIN
+define("TBL_ESTADOS", DB_ADMIN . TBL_ADMIN . "uf");
+define("TBL_CIDADES", DB_ADMIN . TBL_ADMIN . "cidades");
+define("TBL_CLIENTES", DB_ADMIN . TBL_ADMIN . "clientes");
+define("TBL_ENDERECOS", DB_ADMIN . TBL_ADMIN . "enderecos");
+define("TBL_FORNECEDORES", DB_ADMIN . TBL_ADMIN . "fornecedores");
+define("TBL_JOB", DB_ADMIN . TBL_ADMIN . "jobs");
---
+++ model/constantesBanco.php
@@ -0,0 +1,19 @@
+<?php
+//Local
+define('CON_ADMIN_HOST','localhost');
+define("CON_ADMIN_USER","root");
+define("CON_ADMIN_PASS","");
+define("CON_ADMIN_BASE","gcommerce");
+
+/*
+ //servidor
+define('CON_ADMIN_HOST','162.248.241.106');
+define("CON_ADMIN_USER","bienaldo_admin");
+define("CON_ADMIN_PASS","Q1w2e3R4t52@15%&*B!eN@l");
+define("CON_ADMIN_BASE","bienaldo_administradorGl");
+*/
+//Constantes de banco
+define("DB_ADMIN",CON_ADMIN_BASE.".");
+
+//Constantes prefixo sistema
+define("TBL_ADMIN","");
