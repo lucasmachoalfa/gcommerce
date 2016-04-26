b4b0abc8d8c2aacb9f9194679c3babc87a9ba9be
Paulo Sergio <paulo.sergio@agenciaguppy.com.br>
2016-04-26 11:20:07 -0300

    correção da query

--- model/opcaoDao.php
+++ model/opcaoDao.php
@@ -9,20 +9,20 @@ class OpcaoDao extends Banco {
 
     public function listaOpcoes() {
         $conexao = $this->abreConexao();
-        
-        $sql = "SELECT o.idOpcao, o.titulo,
-                FROM ".TBL_OPCOES." o
-                LEFT JOIN ".TBL_VARIACOES."
+
+        $sql = "SELECT o.idOpcao, o.titulo
+                FROM " . TBL_OPCOES . " o
+                LEFT JOIN " . TBL_VARIACAO . " v ON o.idOpcao = v.idOpcao
                 WHERE o.status = 1";
-        
+
         $banco = $conexao->query($sql);
-        
+
         $linhas = array();
-        while($linha = $banco->fetch_assoc()){
+        while ($linha = $banco->fetch_assoc()) {
             $linhas[] = $linha;
         }
-        
-        
+
+
         return $linhas;
         $this->fechaConexao();
     }
