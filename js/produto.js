4d1fd9e18abe18e894d644b180f2cd3d7feb5b26
Paulo Sergio <paulo.sergio@agenciaguppy.com.br>
2016-05-11 00:31:39 -0300

    opções/variações adicionando valores ao envio

--- js/produto.js
+++ js/produto.js
@@ -1,21 +1,33 @@
-
 $(document).ready(function () {
     $("#btnCadProduto").click(function () {
         var data = new FormData();
+        //<detalhes>
+        
+        //</detalhes>
+        
 
-        //estoque e variações
-        validOpcao = new Array();
+        //<estoque e variações>
+        opcoesProdutos = new Array();
         if ($("#variacoesProdutos .row").length) {
             $("#variacoesProdutos .row").each(function () {
                 var idDiv = $(this).attr('id');
-//                validOpcao['row'] = idDiv;
-                $("#"+idDiv+" select").each(function () {
+                var referencia = $("#referencia" + idDiv).val();
+                var quantidade = $("#quantidade" + idDiv).val();
+                var preco = $("#preco" + idDiv).val();
+                var peso = $("#peso" + idDiv).val();
+
+                $("#" + idDiv + " select").each(function () {
                     var valorOpcao = $(this).val();
                     var idOpcao = $(this).attr('id');
 
                     var row = {
                         idDiv: idDiv,
                         idOpcao: idOpcao,
+                        valorOpcao: '',
+                        referencia: '',
+                        quantidade: '',
+                        preco: '',
+                        peso: '',
                         valid: false
                     };
 
@@ -25,12 +37,25 @@ $(document).ready(function () {
                     } else {
                         validateBootstrap(idOpcao, '', 0);
                         row.valid = true;
+                        row.valorOpcao = valorOpcao;
+                        row.referencia = referencia;
+                        row.quantidade = quantidade;
+                        row.preco = preco;
+                        row.peso = peso;
+
                     }
-                    validOpcao.push(row);
+                    opcoesProdutos.push(row);
                 });
-
-
             });
         }
+        
+        var jsonOpcoesProduto = new Array();
+        for (var key in opcoesProdutos) {
+            if (opcoesProdutos[key].valid === true) {
+                jsonOpcoesProduto.push(opcoesProdutos[key]);
+            }
+        }
+        data.append('opcoesProdutos', JSON.stringify(jsonOpcoesProduto));
+        //</estoque e variações>
     });
 });
